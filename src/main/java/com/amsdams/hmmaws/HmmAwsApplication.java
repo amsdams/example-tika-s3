b57ba4369142b38d80f11d1d463ca3830221f21d
ju la <>
2021-07-05 23:12:18 +0200

    s3 with tika

---
+++ src/main/java/com/amsdams/hmmaws/HmmAwsApplication.java
@@ -0,0 +1,13 @@
+package com.amsdams.hmmaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmmAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmmAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amsdams/hmmaws/HmmAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsdams.hmmaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmmAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
