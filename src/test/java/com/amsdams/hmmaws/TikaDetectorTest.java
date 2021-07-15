fee9ea7d510452720c58d29fa9ceaae435d17e3d
amsdams <julien.langeveld@gmail.com>
2021-07-15 13:47:45 +0200

    trying parsers. unit test from cmd wants a mina class

--- src/test/java/com/amsdams/hmmaws/TikaDetectorTest.java
+++ src/test/java/com/amsdams/hmmaws/TikaDetectorTest.java
@@ -1,24 +1,44 @@
 package com.amsdams.hmmaws;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 import org.apache.tika.detect.DefaultDetector;
 import org.apache.tika.detect.Detector;
+import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.mp4.MP4Parser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mp4parser.Box;
+import org.mp4parser.IsoFile;
+import org.mp4parser.boxes.iso14496.part12.HandlerBox;
+import org.mp4parser.boxes.iso14496.part12.MediaBox;
+import org.mp4parser.boxes.iso14496.part12.TrackBox;
+import org.red5.io.mp3.impl.MP3Reader;
+import org.red5.io.mp4.impl.MP4Reader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
@@ -75,13 +95,16 @@ public class TikaDetectorTest {
 	@CsvSource({ "classpath:data/file_example_MP4_480_1_5MG.mp4,video/mp4",
 			"classpath:data/file_example_MP3_700KB.mp3,audio/mpeg",
 			"classpath:data/file-sample_150kB.pdf,application/pdf" })
-	void test_OK(String input, String expected) throws AmazonServiceException, SdkClientException, IOException {
+	void test_OK(String input, String expected)
+			throws AmazonServiceException, SdkClientException, IOException, SAXException, TikaException {
 
 		Resource resource = resourcePatternResolver.getResource(input);
 
 		client.putObject(BUCKET_NAME, KEY, resource.getFile());
 		S3Object s3Object = client.getObject(BUCKET_NAME, KEY);
-		MediaType mediaType = this.getMediaType(s3Object.getObjectContent());
+		InputStream inputStream = s3Object.getObjectContent();
+
+		MediaType mediaType = this.getMediaType(inputStream);
 
 		log.info("mediaType {}", mediaType);
 		Assertions.assertEquals(expected, mediaType.toString());
@@ -98,7 +121,9 @@ public class TikaDetectorTest {
 
 		client.putObject(BUCKET_NAME, KEY, resource.getFile());
 		S3Object s3Object = client.getObject(BUCKET_NAME, KEY);
-		MediaType mediaType = this.getMediaType(s3Object.getObjectContent());
+		InputStream inputStream = s3Object.getObjectContent();
+
+		MediaType mediaType = this.getMediaType(inputStream);
 
 		log.info("mediaType {}", mediaType);
 		Assertions.assertEquals(expected, mediaType.toString());
@@ -119,6 +144,87 @@ public class TikaDetectorTest {
 		Detector detector = new DefaultDetector();
 		Metadata metadata = new Metadata();
 		return detector.detect(new BufferedInputStream(inputStream), metadata);
+
+	}
+
+	void parser(InputStream inputStream) throws IOException, SAXException, TikaException {
+		MP4Parser mp4 = new MP4Parser();
+		ContentHandler handler = new BodyContentHandler();
+		Metadata metadata = new Metadata();
+		ParseContext context = new ParseContext();
+
+		mp4.parse(new BufferedInputStream(inputStream), handler, metadata, context);
+
+		String[] metadataNames = metadata.names();
+
+		for (String name : metadataNames) {
+			System.out.println(name + ": " + metadata.get(name));
+		}
+
+		// metadata.getValues(null)
+	}
+
+	public static final String PREFIX = "stream2file";
+	public static final String SUFFIX = ".tmp";
+
+	public static File stream2file(InputStream in) throws IOException {
+		final File tempFile = File.createTempFile(PREFIX, SUFFIX);
+		tempFile.deleteOnExit();
+		Files.copy(in, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		return tempFile;
+
+	}
+
+	@Test
+	void test_parser_OK_MP4()
+			throws AmazonServiceException, SdkClientException, IOException, SAXException, TikaException {
+
+		Resource resource = resourcePatternResolver.getResource("classpath:data/file_example_MP4_480_1_5MG.mp4");
+
+		client.putObject(BUCKET_NAME, KEY, resource.getFile());
+		S3Object s3Object = client.getObject(BUCKET_NAME, KEY);
+		InputStream inputStream = s3Object.getObjectContent();
+		ReadableByteChannel in = Channels.newChannel(inputStream);
+
+		IsoFile isoFile = new IsoFile(in);
+		List<TrackBox> trackBoxes = isoFile.getMovieBox().getBoxes(TrackBox.class);
+
+		for (TrackBox trackBox : trackBoxes) {
+			MediaBox mdia = trackBox.getMediaBox();
+			log.info("mdia {}", mdia.toString());
+			HandlerBox hdlr = mdia.getHandlerBox(); // hdlr
+			log.info("hdlr {}", hdlr.toString());
+		}
+
+	}
+
+	@Test
+	void test_parser_OK_MP4_RED5()
+			throws AmazonServiceException, SdkClientException, IOException, SAXException, TikaException {
+
+		Resource resource = resourcePatternResolver.getResource("classpath:data/file_example_MP4_480_1_5MG.mp4");
+
+		client.putObject(BUCKET_NAME, KEY, resource.getFile());
+		S3Object s3Object = client.getObject(BUCKET_NAME, KEY);
+		InputStream inputStream = s3Object.getObjectContent();
+		MP4Reader mp4Reader = new MP4Reader(stream2file(inputStream));
+		Assertions.assertEquals(1570024, mp4Reader.getTotalBytes());
+		Assertions.assertEquals("mp4a", mp4Reader.getAudioCodecId());
+		Assertions.assertEquals("avc1", mp4Reader.getVideoCodecId());
+
+	}
+
+	@Test
+	void test_parser_OK_MP3_RED5()
+			throws AmazonServiceException, SdkClientException, IOException, SAXException, TikaException {
+
+		Resource resource = resourcePatternResolver.getResource("classpath:data/file_example_MP3_700KB.mp3");
+
+		client.putObject(BUCKET_NAME, KEY, resource.getFile());
+		S3Object s3Object = client.getObject(BUCKET_NAME, KEY);
+		InputStream inputStream = s3Object.getObjectContent();
+		MP3Reader mp3Reader = new MP3Reader(stream2file(inputStream));
+		Assertions.assertEquals(123, mp3Reader.getTotalBytes());
 
 	}
 }
